c9421167fe288fcf381499585c3cc3dc5bdb4e58
OmkarSawantt <omkarsawntttt4@gmail.com>
2024-07-13 09:44:22 +0530

    First Commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express=require('express')
+const cors=require('cors')
+require('dotenv').config()
+
+const app=express()
+app.use(cors({
+    origin:process.env.FRONTEND_URL
+}))
+
+const PORT=process.env.PORT || 8000
+
+app.get('/',(req,res)=>{
+    res.json({
+        message:"server running at "+PORT
+    })
+})
+app.listen(PORT,()=>{
+    console.log("server running at "+PORT);
+})
